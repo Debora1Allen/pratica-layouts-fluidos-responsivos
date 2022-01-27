5cda730685893d3ade10abe56299366fabde6687
Debora Alina <deboraaaline21@gmail.com>
2022-01-27 00:03:15 -0300

    finalizado

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -5,9 +5,11 @@ function Footer() {
         <footer>
             <div className="footer-left">
                 <img src={Logo} />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Volutpat est velit egestas dui id. Id semper risus in hendrerit. Dictum fusce ut placerat orci. Ultrices in iaculis nunc sed augue lacus. Dui id </p>
             </div>
-            <div className="footer-right">
 
+            <div className="footer-rigth">
+                <p>Adipiscing commodo elit at imperdiet dui accumsan sit amet. Nascetur ridiculus mus mauris vitae. Donec ultrices tincidunt arcu non sodales neque sodales ut etiam. Tempus imperdiet nulla malesuada pellentesque elit. Erat imperdiet sed euismod nisi porta lorem. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh. Velit sed ullamcorper morbi tincidunt ornare. Blandit aliquam etiam erat velit scelerisque. Et sollicitudin ac orci phasellus egestas. Gravida rutrum quisque non tellus. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Diam vel quam elementum pulvinar etiam non quam.</p>
             </div>
         </footer>
 
--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,7 +1,23 @@
-import './style.css'
+import './style.css';
+import menu from '../../assest/menu.svg';
+import close from '../../assest/close.svg';
+import { useEffect, useState } from 'react';
 function Menu() {
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.querySelector('body').style.overflow = open ? 'hidden' : 'scroll';
+    }, [open]);
     return (
-        <div >
+        <div className={`${open && 'fullscreen-menu'}`}>
+            {open ? <img
+                className='icon-close'
+                style={{ display: `${open} ? 'block' : 'none'` }}
+                onClick={() => setOpen(false)} src={close} alt='close icon'
+            /> : <img
+                className='icon-menu'
+                onClick={() => setOpen(true)} src={menu} alt='menu icon'
+            />}
             <ul className="menu">
                 <li>Ação</li>
                 <li>Comédia</li>
